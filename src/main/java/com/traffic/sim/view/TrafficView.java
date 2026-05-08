6a2f4891ec160b67b5722d7afae02e769f77f240
img37 <hrbhmody@gmail.com>
2026-05-08 12:23:31 +0300

    Update TrafficView.java

--- src/main/java/com/traffic/sim/view/TrafficView.java
+++ src/main/java/com/traffic/sim/view/TrafficView.java
@@ -1,249 +1,162 @@
 package com.traffic.sim.view;
 
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+import java.util.*;
 
 public class TrafficView {
+    private final Group root;
+    private final PerspectiveCamera camera = new PerspectiveCamera(true);
+    private final Group carGroup = new Group();
+    private final Map<Integer, Group> npcCars = new HashMap<>();
+    private final List<Sphere[]> signals = new ArrayList<>();
 
-    // إشارات المرور
-    private Circle topRed, topYellow, topGreen;
-    private Circle bottomRed, bottomYellow, bottomGreen;
-    private Circle leftRed, leftYellow, leftGreen;
-    private Circle rightRed, rightYellow, rightGreen;
-
-    // سيارات
-    private Rectangle carTop;
-    private Rectangle carBottom;
-    private Rectangle carLeft;
-    private Rectangle carRight;
-
-    // زر تشغيل
-    private Button startButton;
-
-    public TrafficView(Stage stage) {
-
-        Pane root = new Pane();
-        root.setPrefSize(800, 800);
-
-        // الخلفية
-        root.setStyle("-fx-background-color: lightgray;");
-
-        // ===== الطرق =====
-
-        Rectangle verticalRoad = new Rectangle(300, 0, 200, 800);
-        verticalRoad.setFill(Color.BLACK);
-
-        Rectangle horizontalRoad = new Rectangle(0, 300, 800, 200);
-        horizontalRoad.setFill(Color.BLACK);
-
-        root.getChildren().addAll(verticalRoad, horizontalRoad);
-
-        // ===== الخطوط الصفراء =====
-
-        Rectangle vLine = new Rectangle(395, 0, 10, 800);
-        vLine.setFill(Color.GOLD);
-
-        Rectangle hLine = new Rectangle(0, 395, 800, 10);
-        hLine.setFill(Color.GOLD);
-
-        root.getChildren().addAll(vLine, hLine);
-
-        // ===== إشارات المرور =====
-
-        // فوق
-        topRed = createLight(340, 250, Color.RED);
-        topYellow = createLight(370, 250, Color.DARKGOLDENROD);
-        topGreen = createLight(400, 250, Color.DARKGREEN);
-
-        // تحت
-        bottomRed = createLight(340, 520, Color.DARKRED);
-        bottomYellow = createLight(370, 520, Color.DARKGOLDENROD);
-        bottomGreen = createLight(400, 520, Color.DARKGREEN);
-
-        // يسار
-        leftRed = createLight(250, 340, Color.DARKRED);
-        leftYellow = createLight(250, 370, Color.DARKGOLDENROD);
-        leftGreen = createLight(250, 400, Color.DARKGREEN);
-
-        // يمين
-        rightRed = createLight(520, 340, Color.DARKRED);
-        rightYellow = createLight(520, 370, Color.DARKGOLDENROD);
-        rightGreen = createLight(520, 400, Color.DARKGREEN);
-
-        root.getChildren().addAll(
-                topRed, topYellow, topGreen,
-                bottomRed, bottomYellow, bottomGreen,
-                leftRed, leftYellow, leftGreen,
-                rightRed, rightYellow, rightGreen);
-
-        // ===== السيارات =====
-
-        carTop = createCar(360, 120, Color.BLUE);
-        carBottom = createCar(420, 620, Color.GREEN);
-        carLeft = createCarHorizontal(120, 360, Color.ORANGE);
-        carRight = createCarHorizontal(620, 420, Color.PURPLE);
-
-        root.getChildren().addAll(carTop, carBottom, carLeft, carRight);
-
-        // ===== زر التشغيل =====
-
-        startButton = new Button("Start Simulation");
-        startButton.setLayoutX(320);
-        startButton.setLayoutY(730);
-
-        root.getChildren().add(startButton);
-
-        Scene scene = new Scene(root);
-
-        stage.setScene(scene);
+    public TrafficView(Group root) {
+        this.root = root;
+        setupCamera();
+        setupLights();
+        buildProfessionalEnvironment();
+        createUltraCar(carGroup, Color.web("#b22222")); // سيارتك (أحمر ملكي)
     }
 
-    // إنشاء إشارة
-    private Circle createLight(double x, double y, Color color) {
-        Circle light = new Circle(12);
-        light.setCenterX(x);
-        light.setCenterY(y);
-        light.setFill(color);
-        return light;
+    private void setupCamera() {
+        camera.setNearClip(1.0); camera.setFarClip(40000.0); camera.setFieldOfView(45);
+        root.getChildren().add(camera);
     }
 
-    // سيارة عمودية
-    private Rectangle createCar(double x, double y, Color color) {
-        Rectangle car = new Rectangle(35, 60);
-        car.setX(x);
-        car.setY(y);
-        car.setFill(color);
-        return car;
+    private void setupLights() {
+        DirectionalLight sun = new DirectionalLight(Color.rgb(255, 250, 220));
+        sun.setDirection(new javafx.geometry.Point3D(0.4, 1, 0.6));
+        root.getChildren().addAll(sun, new AmbientLight(Color.rgb(120, 120, 150)));
     }
 
-    // سيارة أفقية
-    private Rectangle createCarHorizontal(double x, double y, Color color) {
-        Rectangle car = new Rectangle(60, 35);
-        car.setX(x);
-        car.setY(y);
-        car.setFill(color);
-        return car;
+    private void buildProfessionalEnvironment() {
+        // الأرضية الخضراء
+        Box grass = new Box(30000, 1, 40000);
+        grass.setTranslateY(35); grass.setMaterial(new PhongMaterial(Color.web("#347c2c")));
+        
+        // الشارع مع خطوط المسارات
+        Group roadGroup = new Group();
+        Box asphalt = new Box(1000, 2, 40000);
+        asphalt.setMaterial(new PhongMaterial(Color.web("#1a1a1a")));
+        roadGroup.getChildren().add(asphalt);
+
+        // إضافة خطوط الشارع (الخطوط البيضاء المقطعة)
+        for (int z = -20000; z < 20000; z += 400) {
+            Box line = new Box(15, 3, 150);
+            line.setTranslateZ(z);
+            line.setMaterial(new PhongMaterial(Color.WHITE));
+            roadGroup.getChildren().add(line);
+        }
+        root.getChildren().addAll(grass, roadGroup);
+
+        // الرصيف والبيوت بتفاصيل (الباب والطاقة)
+        for (int x : new int[]{-650, 650}) {
+            // رصيف مخطط (أسود وأبيض)
+            for (int z = -20000; z < 20000; z += 200) {
+                Box curb = new Box(120, 20, 200);
+                curb.setTranslateX(x > 0 ? 560 : -560); curb.setTranslateZ(z); curb.setTranslateY(25);
+                curb.setMaterial(new PhongMaterial((z/200) % 2 == 0 ? Color.web("#333333") : Color.WHITE));
+                root.getChildren().add(curb);
+            }
+            // البيوت الواقعية
+            for (int z = -18000; z < 18000; z += 3500) {
+                root.getChildren().add(createRealisticBuilding(x * 1.8, z));
+            }
+        }
+        for (int z : new int[]{4000, 10000, 16000}) createModernSignal(580, z);
     }
 
-    // ===== Getters =====
+    private Group createRealisticBuilding(double x, double z) {
+        Group house = new Group();
+        Box body = new Box(800, 1000, 800);
+        body.setMaterial(new PhongMaterial(Color.web("#f5f5dc")));
+        
+        // الباب (واقعي)
+        Box door = new Box(10, 200, 100);
+        door.setTranslateX(x > 0 ? -405 : 405); door.setTranslateY(400);
+        door.setMaterial(new PhongMaterial(Color.web("#4b2d12")));
 
-    public Button getStartButton() {
-        return startButton;
+        // الطاقة (الشبابيك) مع براويز
+        for (int y = -350; y <= 150; y += 250) {
+            for (int wz = -250; wz <= 250; wz += 500) {
+                Box window = new Box(810, 120, 120);
+                window.setTranslateY(y); window.setTranslateZ(wz);
+                window.setMaterial(new PhongMaterial(Color.web("#add8e6"))); // لون زجاجي
+                house.getChildren().add(window);
+            }
+        }
+        house.setTranslateX(x); house.setTranslateZ(z); house.setTranslateY(-465);
+        house.getChildren().add(body); house.getChildren().add(door);
+        return house;
     }
 
-    public Circle getTopRed() {
-        return topRed;
+    private void createUltraCar(Group g, Color bodyColor) {
+        // جسم السيارة الانسيابي
+        Box mainBody = new Box(90, 35, 190); mainBody.setMaterial(new PhongMaterial(bodyColor));
+        Box cabin = new Box(70, 30, 100); cabin.setTranslateY(-30); cabin.setTranslateZ(-15);
+        cabin.setMaterial(new PhongMaterial(Color.web("#1a1a1a")));
+
+        // الكفرات (4 كفرات بارزة)
+        double[][] wPos = {{-50, 20, 60}, {50, 20, 60}, {-50, 20, -60}, {50, 20, -60}};
+        for (double[] p : wPos) {
+            Cylinder wheel = new Cylinder(25, 18);
+            wheel.setRotationAxis(javafx.scene.transform.Rotate.Z_AXIS); wheel.setRotate(90);
+            wheel.setTranslateX(p[0]); wheel.setTranslateY(p[1]); wheel.setTranslateZ(p[2]);
+            wheel.setMaterial(new PhongMaterial(Color.web("#111111")));
+            g.getChildren().add(wheel);
+        }
+
+        // الأنوار الأمامية (قوية)
+        Sphere lightL = new Sphere(12); lightL.setTranslateX(-30); lightL.setTranslateY(-5); lightL.setTranslateZ(95);
+        lightL.setMaterial(new PhongMaterial(Color.web("#ffffe0")));
+        Sphere lightR = new Sphere(12); lightR.setTranslateX(30); lightR.setTranslateY(-5); lightR.setTranslateZ(95);
+        lightR.setMaterial(new PhongMaterial(Color.web("#ffffe0")));
+
+        g.getChildren().addAll(mainBody, cabin, lightL, lightR);
+        if (g == carGroup) root.getChildren().add(carGroup);
     }
 
-    public Circle getTopYellow() {
-        return topYellow;
+    public void updatePlayer(double x, double z) {
+        carGroup.setTranslateX(x); carGroup.setTranslateZ(z);
+        camera.setTranslateX(x); camera.setTranslateZ(z - 1200);
+        camera.setTranslateY(-700);
+        camera.setRotationAxis(javafx.scene.transform.Rotate.X_AXIS); camera.setRotate(-25);
     }
 
-    public Circle getTopGreen() {
-        return topGreen;
+    public void updateNPC(int id, double x, double z) {
+        Group npc = npcCars.computeIfAbsent(id, k -> {
+            Group g = new Group();
+            createUltraCar(g, Color.DARKSLATEGRAY);
+            root.getChildren().add(g);
+            return g;
+        });
+        npc.setTranslateX(x); npc.setTranslateZ(z);
     }
 
-    public Circle getBottomRed() {
-        return bottomRed;
+    private void createModernSignal(double x, double z) {
+        Group g = new Group();
+        Box pole = new Box(15, 550, 15); pole.setMaterial(new PhongMaterial(Color.web("#444444")));
+        Box head = new Box(80, 240, 80); head.setTranslateY(-410); head.setMaterial(new PhongMaterial(Color.web("#111111")));
+        Sphere r = lamp(-480); Sphere y = lamp(-410); Sphere gL = lamp(-340);
+        signals.add(new Sphere[]{r, y, gL});
+        g.getChildren().addAll(pole, head, r, y, gL);
+        g.setTranslateX(x); g.setTranslateZ(z);
+        root.getChildren().add(g);
     }
 
-    public Circle getBottomYellow() {
-        return bottomYellow;
+    private Sphere lamp(double y) {
+        Sphere s = new Sphere(24); s.setTranslateY(y); s.setTranslateZ(-42);
+        s.setMaterial(new PhongMaterial(Color.BLACK)); return s;
     }
 
-    public Circle getBottomGreen() {
-        return bottomGreen;
-    }
-
-    public Circle getLeftRed() {
-        return leftRed;
-    }
-
-    public Circle getLeftYellow() {
-        return leftYellow;
-    }
-
-    public Circle getLeftGreen() {
-        return leftGreen;
-    }
-
-    public Circle getRightRed() {
-        return rightRed;
-    }
-
-    public Circle getRightYellow() {
-        return rightYellow;
-    }
-
-    public Circle getRightGreen() {
-        return rightGreen;
-    }
-
-    public Rectangle getCarTop() {
-        return carTop;
-    }
-
-    public Rectangle getCarBottom() {
-        return carBottom;
-    }
-
-    public Rectangle getCarLeft() {
-        return carLeft;
-    }
-
-    public Rectangle getCarRight() {
-        return carRight;
-    }
-
-    public void updateLights(String state) {
-
-        // Top layer
-
-        topRed.setFill(Color.DARKRED);
-        topYellow.setFill(Color.DARKGOLDENROD);
-        topGreen.setFill(Color.DARKGREEN);
-
-        // bottom layer
-
-        bottomRed.setFill(Color.DARKRED);
-        bottomYellow.setFill(Color.DARKGOLDENROD);
-        bottomGreen.setFill(Color.DARKGREEN);
-
-        // left layer
-
-        leftRed.setFill(Color.DARKRED);
-        leftYellow.setFill(Color.DARKGOLDENROD);
-        leftGreen.setFill(Color.DARKGREEN);
-
-        // right layer
-
-        rightRed.setFill(Color.DARKRED);
-        rightYellow.setFill(Color.DARKGOLDENROD);
-        rightGreen.setFill(Color.DARKGREEN);
-
-        switch (state.toUpperCase()) {
-            case "RED":
-                topRed.setFill(Color.RED);
-                bottomRed.setFill(Color.RED);
-                rightRed.setFill(Color.RED);
-                leftRed.setFill(Color.RED);
-                break;
-            case "YELLOW":
-                topYellow.setFill(Color.YELLOW);
-                bottomYellow.setFill(Color.YELLOW);
-                rightYellow.setFill(Color.YELLOW);
-                leftYellow.setFill(Color.YELLOW);
-                break;
-            case "GREEN":
-                topGreen.setFill(Color.LIME);
-                bottomGreen.setFill(Color.LIME);
-                rightGreen.setFill(Color.LIME);
-                leftGreen.setFill(Color.LIME);
+    public void updateSignals(int state) {
+        for (Sphere[] s : signals) {
+            s[0].setMaterial(new PhongMaterial(state == 0 ? Color.RED : Color.web("#200000")));
+            s[1].setMaterial(new PhongMaterial(state == 1 ? Color.YELLOW : Color.web("#202000")));
+            s[2].setMaterial(new PhongMaterial(state == 2 ? Color.LIME : Color.web("#002000")));
         }
     }
+    public PerspectiveCamera getCamera() { return camera; }
 }
